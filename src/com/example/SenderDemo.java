50d21c3738a9eb4360bc590958697a4bf8020bd7
Ritesh Tyagi <hi2tyagi@yahoo.com>
2019-02-04 16:17:50 +0530

    Added Samples

---
+++ src/com/example/SenderDemo.java
@@ -0,0 +1,41 @@
+package com.example;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+public class SenderDemo {
+
+	public static void main(String[] args) throws Exception {
+		new SenderDemo();
+	}
+
+	public SenderDemo() throws Exception {
+
+		String ROUTING_KEY1 = "java";
+		String ROUTING_KEY2 = "spring";
+		
+
+		ConnectionFactory factory = new ConnectionFactory();
+		factory.setHost("localhost");
+		factory.setPort(8000);
+		factory.setUsername("guest");
+		factory.setPassword("guest");
+		
+		Connection connection = factory.newConnection();
+		Channel channel = connection.createChannel();
+		
+	
+		channel.basicPublish("Books", ROUTING_KEY1, null, "Java in Action".getBytes("UTF-8"));
+		channel.basicPublish("Books", ROUTING_KEY1, null, "Java in 21 Days".getBytes("UTF-8"));
+
+		channel.basicPublish("Books", ROUTING_KEY2, null, "Spring in Action".getBytes("UTF-8"));
+		channel.basicPublish("Books", ROUTING_KEY2, null, "Spring in 5 Days".getBytes("UTF-8"));
+
+		System.out.println("Done");
+
+		channel.close();
+		connection.close();
+
+	}
+}
